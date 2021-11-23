7542112bd3caf15bce38823a8d1d37a412689663
Hordiychuk Vitaliy <vitalikokgo@gmail.com>
2021-11-23 06:19:19 +0300

    .

---
+++ WinFormsApp1/Form1.Designer.cs
@@ -0,0 +1,248 @@
+﻿using System.Xml.Linq;
+namespace WinFormsApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.nameBox = new System.Windows.Forms.ComboBox();
+            this.facultyBox = new System.Windows.Forms.ComboBox();
+            this.cathedraBox = new System.Windows.Forms.ComboBox();
+            this.nameCheckBox = new System.Windows.Forms.CheckBox();
+            this.facultyCheckBox = new System.Windows.Forms.CheckBox();
+            this.cathedraСheckBox = new System.Windows.Forms.CheckBox();
+            this.courseCheckBox = new System.Windows.Forms.CheckBox();
+            this.courseBox = new System.Windows.Forms.ComboBox();
+            this.dataBox = new System.Windows.Forms.ComboBox();
+            this.dataCheckBox = new System.Windows.Forms.CheckBox();
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.tableLayoutPanel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // nameBox
+            // 
+            this.nameBox.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Left | System.Windows.Forms.AnchorStyles.Right)));
+            this.nameBox.FormattingEnabled = true;
+            this.nameBox.Location = new System.Drawing.Point(124, 14);
+            this.nameBox.Name = "nameBox";
+            this.nameBox.Size = new System.Drawing.Size(220, 28);
+            this.nameBox.TabIndex = 0;
+            this.nameBox.Click += new System.EventHandler(this.nameBox_Click);
+            // 
+            // facultyBox
+            // 
+            this.facultyBox.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Left | System.Windows.Forms.AnchorStyles.Right)));
+            this.facultyBox.FormattingEnabled = true;
+            this.facultyBox.Location = new System.Drawing.Point(124, 70);
+            this.facultyBox.Name = "facultyBox";
+            this.facultyBox.Size = new System.Drawing.Size(220, 28);
+            this.facultyBox.TabIndex = 1;
+            this.facultyBox.Click += new System.EventHandler(this.faculty_Click);
+            // 
+            // cathedraBox
+            // 
+            this.cathedraBox.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Left | System.Windows.Forms.AnchorStyles.Right)));
+            this.cathedraBox.FormattingEnabled = true;
+            this.cathedraBox.Location = new System.Drawing.Point(124, 126);
+            this.cathedraBox.Name = "cathedraBox";
+            this.cathedraBox.Size = new System.Drawing.Size(220, 28);
+            this.cathedraBox.TabIndex = 2;
+            this.cathedraBox.Click += new System.EventHandler(this.cathedra_Click);
+            // 
+            // nameCheckBox
+            // 
+            this.nameCheckBox.AutoSize = true;
+            this.nameCheckBox.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.nameCheckBox.Location = new System.Drawing.Point(3, 3);
+            this.nameCheckBox.Name = "nameCheckBox";
+            this.nameCheckBox.Size = new System.Drawing.Size(115, 50);
+            this.nameCheckBox.TabIndex = 3;
+            this.nameCheckBox.Text = "Ім\'я";
+            this.nameCheckBox.UseVisualStyleBackColor = true;
+            this.nameCheckBox.CheckedChanged += new System.EventHandler(this.nameCheckBox_CheckedChanged);
+            // 
+            // facultyCheckBox
+            // 
+            this.facultyCheckBox.AutoSize = true;
+            this.facultyCheckBox.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.facultyCheckBox.Location = new System.Drawing.Point(3, 59);
+            this.facultyCheckBox.Name = "facultyCheckBox";
+            this.facultyCheckBox.Size = new System.Drawing.Size(115, 50);
+            this.facultyCheckBox.TabIndex = 4;
+            this.facultyCheckBox.Text = "Факультет";
+            this.facultyCheckBox.UseVisualStyleBackColor = true;
+            this.facultyCheckBox.CheckedChanged += new System.EventHandler(this.facultyCheckBox_CheckedChanged);
+            // 
+            // cathedraСheckBox
+            // 
+            this.cathedraСheckBox.AutoSize = true;
+            this.cathedraСheckBox.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.cathedraСheckBox.Location = new System.Drawing.Point(3, 115);
+            this.cathedraСheckBox.Name = "cathedraСheckBox";
+            this.cathedraСheckBox.Size = new System.Drawing.Size(115, 50);
+            this.cathedraСheckBox.TabIndex = 5;
+            this.cathedraСheckBox.Text = "Кафедра";
+            this.cathedraСheckBox.UseVisualStyleBackColor = true;
+            this.cathedraСheckBox.CheckedChanged += new System.EventHandler(this.cathedraСheckBox_CheckedChanged);
+            // 
+            // courseCheckBox
+            // 
+            this.courseCheckBox.AutoSize = true;
+            this.courseCheckBox.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.courseCheckBox.Location = new System.Drawing.Point(3, 171);
+            this.courseCheckBox.Name = "courseCheckBox";
+            this.courseCheckBox.Size = new System.Drawing.Size(115, 50);
+            this.courseCheckBox.TabIndex = 6;
+            this.courseCheckBox.Text = "Курс";
+            this.courseCheckBox.UseVisualStyleBackColor = true;
+            this.courseCheckBox.CheckedChanged += new System.EventHandler(this.courseCheckBox_CheckedChanged);
+            // 
+            // courseBox
+            // 
+            this.courseBox.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Left | System.Windows.Forms.AnchorStyles.Right)));
+            this.courseBox.FormattingEnabled = true;
+            this.courseBox.Location = new System.Drawing.Point(124, 182);
+            this.courseBox.Name = "courseBox";
+            this.courseBox.Size = new System.Drawing.Size(220, 28);
+            this.courseBox.TabIndex = 7;
+            this.courseBox.Click += new System.EventHandler(this.course_Click);
+            // 
+            // dataBox
+            // 
+            this.dataBox.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Left | System.Windows.Forms.AnchorStyles.Right)));
+            this.dataBox.FormattingEnabled = true;
+            this.dataBox.Location = new System.Drawing.Point(124, 239);
+            this.dataBox.Name = "dataBox";
+            this.dataBox.Size = new System.Drawing.Size(220, 28);
+            this.dataBox.TabIndex = 8;
+            this.dataBox.Click += new System.EventHandler(this.data_Click);
+            // 
+            // dataCheckBox
+            // 
+            this.dataCheckBox.AutoSize = true;
+            this.dataCheckBox.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.dataCheckBox.Location = new System.Drawing.Point(3, 227);
+            this.dataCheckBox.Name = "dataCheckBox";
+            this.dataCheckBox.Size = new System.Drawing.Size(115, 52);
+            this.dataCheckBox.TabIndex = 9;
+            this.dataCheckBox.Text = "Дата";
+            this.dataCheckBox.UseVisualStyleBackColor = true;
+            this.dataCheckBox.CheckedChanged += new System.EventHandler(this.dataCheckBox_CheckedChanged);
+            // 
+            // tableLayoutPanel1
+            // 
+            this.tableLayoutPanel1.ColumnCount = 2;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 35F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 65F));
+            this.tableLayoutPanel1.Controls.Add(this.nameCheckBox, 0, 0);
+            this.tableLayoutPanel1.Controls.Add(this.dataBox, 1, 4);
+            this.tableLayoutPanel1.Controls.Add(this.dataCheckBox, 0, 4);
+            this.tableLayoutPanel1.Controls.Add(this.courseBox, 1, 3);
+            this.tableLayoutPanel1.Controls.Add(this.facultyCheckBox, 0, 1);
+            this.tableLayoutPanel1.Controls.Add(this.cathedraBox, 1, 2);
+            this.tableLayoutPanel1.Controls.Add(this.cathedraСheckBox, 0, 2);
+            this.tableLayoutPanel1.Controls.Add(this.facultyBox, 1, 1);
+            this.tableLayoutPanel1.Controls.Add(this.courseCheckBox, 0, 3);
+            this.tableLayoutPanel1.Controls.Add(this.nameBox, 1, 0);
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(12, 12);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            this.tableLayoutPanel1.RowCount = 5;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 20F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 20F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 20F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 20F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 20F));
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(347, 282);
+            this.tableLayoutPanel1.TabIndex = 10;
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(365, 6);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(423, 432);
+            this.richTextBox1.TabIndex = 11;
+            this.richTextBox1.Text = "";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(214, 345);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(94, 29);
+            this.button1.TabIndex = 12;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(49, 344);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(94, 29);
+            this.button2.TabIndex = 13;
+            this.button2.Text = "button2";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.tableLayoutPanel1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.tableLayoutPanel1.ResumeLayout(false);
+            this.tableLayoutPanel1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private  System.Windows.Forms.ComboBox nameBox;
+        private System.Windows.Forms.ComboBox facultyBox;
+        private System.Windows.Forms.ComboBox cathedraBox;
+        private System.Windows.Forms.CheckBox nameCheckBox;
+        private System.Windows.Forms.CheckBox facultyCheckBox;
+        private System.Windows.Forms.CheckBox cathedraСheckBox;
+        private System.Windows.Forms.CheckBox courseCheckBox;
+        private System.Windows.Forms.ComboBox courseBox;
+        private System.Windows.Forms.ComboBox dataBox;
+        private System.Windows.Forms.CheckBox dataCheckBox;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+    }
+}
+
---
+++ WinFormsApp1/Form1.cs
@@ -0,0 +1,293 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Xml.Linq;
+using System.Xml.Xsl;
+namespace WinFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            SetComboBoxItems();
+        }
+        static class ActiveParametrs
+        {
+            public static bool nameActive=false;
+            public static bool facultyActive=false;
+            public static bool cathedraActive=false;
+            public static bool courseActive=false;
+            public static bool dataActive=false;
+        }
+        public class People
+        {
+            public string name;
+            public string faculty;
+            public string cathedra;
+            public string course;
+            public string data;
+            public People(string n, string f, string cath, string cour, string d)
+            {
+                name = n;
+                faculty = f;
+                cathedra = cath;
+                course = cour;
+                data = d;
+            }
+            public People()
+            {
+                name = "";
+                faculty = "";
+                cathedra = "";
+                course = "";
+                data = "";
+            }
+            public string String()
+            {
+
+                return name+'\n'+faculty+'\n'+cathedra+'\n'+course+'\n'+data;
+            }
+        }
+        class Context
+        {
+            private IStrategy _strategy;
+            public Context() { }
+            public Context(IStrategy strategy)
+            {
+                this._strategy = strategy;
+            }
+            public void SetStrategy(IStrategy strategy)
+            {
+                this._strategy = strategy;
+            }
+            public string SearchResult(People people)
+            {
+                string result="";
+                var p = _strategy.Search(people);
+                foreach (var i in p)
+                {
+                    result += i.String() + '\n';
+                }
+                return result;
+            }
+            public void TransformFile(People people)
+            {
+                XslCompiledTransform xslt = new XslCompiledTransform();
+                string f1 = "C:\\Users\\Веталь\\Desktop\\С#\\Paterns\\XSLFile1.xsl";
+                xslt.Load(f1);
+                string f2 = "C:\\Users\\Веталь\\Desktop\\С#\\Paterns\\File112.xml";
+                XDocument xDoc = _strategy.CreateFile(people);
+                xDoc.Save("C:\\Users\\Веталь\\Desktop\\С#\\Paterns\\File112.xml");
+                string f3 = "C:\\Users\\Веталь\\Desktop\\С#\\Paterns\\people112.html";
+                xslt.Transform(f2, f3);
+            }
+        }
+        public interface IStrategy
+        {
+            public List<People> Search(People people);
+            public XDocument CreateFile(People people);  
+        }
+        class linqStrategy : IStrategy
+        {
+            public List<People> Search(People p)
+            {
+                List<People> Result = new List<People>();
+                XDocument xDoc = new XDocument();
+                xDoc = XDocument.Load("C:\\Users\\Веталь\\Desktop\\С#\\Paterns\\File.xml");
+                foreach (XElement people in xDoc.Element("hotel").Elements("people"))
+                {
+                    XAttribute firstname = people.Attribute("firstname");
+                    XAttribute secondname = people.Attribute("secondname");
+                    XAttribute patronymic = people.Attribute("patronymic");
+                    XElement faculty = people.Element("faculty");
+                    XElement cathedra = people.Element("cathedra");
+                    XElement course = people.Element("course");
+                    XElement data = people.Element("data");
+                    string name = secondname.Value + " " + firstname.Value + " " + patronymic.Value;
+
+                    if (Search(name, p.name, ActiveParametrs.nameActive) && Search(faculty.Value, p.faculty, ActiveParametrs.facultyActive)
+                        && Search(cathedra.Value, p.cathedra, ActiveParametrs.cathedraActive)&& Search(course.Value, p.course, ActiveParametrs.courseActive)
+                        && Search(data.Value, p.data, ActiveParametrs.dataActive)) 
+                    {
+                        Result.Add(new People(name, faculty.Value, cathedra.Value, course.Value, data.Value));
+                    }
+                }
+                return Result;
+            }
+            public XDocument CreateFile(People p)
+            {
+                XDocument Result = new XDocument();
+                XDocument xDoc = new XDocument();
+                XElement hotel = new XElement("hotel");
+                xDoc = XDocument.Load("C:\\Users\\Веталь\\Desktop\\С#\\Paterns\\File.xml");
+                foreach (XElement people in xDoc.Element("hotel").Elements("people"))
+                {
+                    XAttribute firstname = people.Attribute("firstname");
+                    XAttribute secondname = people.Attribute("secondname");
+                    XAttribute patronymic = people.Attribute("patronymic");
+                    XElement faculty = people.Element("faculty");
+                    XElement cathedra = people.Element("cathedra");
+                    XElement course = people.Element("course");
+                    XElement data = people.Element("data");
+                    string name = secondname.Value + " " + firstname.Value + " " + patronymic.Value;
+
+                    if (Search(name, p.name, ActiveParametrs.nameActive) && Search(faculty.Value, p.faculty, ActiveParametrs.facultyActive)
+                        && Search(cathedra.Value, p.cathedra, ActiveParametrs.cathedraActive) && Search(course.Value, p.course, ActiveParametrs.courseActive)
+                        && Search(data.Value, p.data, ActiveParametrs.dataActive))
+                    {
+                        XElement Pe = new XElement("people");
+                        Pe.Add(patronymic);
+                        Pe.Add(secondname);
+                        Pe.Add(firstname);
+                        Pe.Add(faculty);
+                        Pe.Add(cathedra);
+                        Pe.Add(course);
+                        Pe.Add(data);
+                        hotel.Add(Pe);
+                    }
+                }
+                Result.Add(hotel);
+                return Result;
+            }
+            bool Search(string parametr1, string parametr2, bool Active)
+            {
+                if (Active == false) return true;
+                return parametr1 == parametr2;
+            }
+        }
+        class domStrategy : IStrategy
+        {
+            public List<People> Search(People people)
+            {
+                return null;
+            }
+            public XDocument CreateFile(People people)
+            { return null; }
+        }
+        class saxStrategy : IStrategy
+        {
+            public List<People> Search(People people)
+            {
+                return null;
+            }
+            public XDocument CreateFile(People people)
+            { return null; }
+        }
+ 
+        private void button1_Click(object sender, EventArgs e)
+        {
+            richTextBox1.Text = " ";
+            linqStrategy linq = new linqStrategy();
+            Context context = new Context(linq);
+            richTextBox1.Text = context.SearchResult(new People(nameBox.Text,facultyBox.Text,cathedraBox.Text,courseBox.Text,dataBox.Text));
+        }
+        public void NewComboBoxItems()
+        {
+            nameBox.Items.Clear();
+            facultyBox.Items.Clear();
+            cathedraBox.Items.Clear();
+            dataBox.Items.Clear();
+            courseBox.Items.Clear();
+            SetComboBoxItems();
+        }
+        public void SetComboBoxItems()
+        {
+            XDocument xDoc = new XDocument();
+            xDoc = XDocument.Load("C:\\Users\\Веталь\\Desktop\\С#\\Paterns\\File.xml");
+            foreach (XElement people in xDoc.Element("hotel").Elements("people"))
+            {
+                XAttribute firstname = people.Attribute("firstname");
+                XAttribute secondname = people.Attribute("secondname");
+                XAttribute patronymic = people.Attribute("patronymic");
+                XElement faculty = people.Element("faculty");
+                XElement cathedra = people.Element("cathedra");
+                XElement course = people.Element("course");
+                XElement data = people.Element("data");
+                string name = secondname.Value + " " + firstname.Value + " " + patronymic.Value;
+                addItems(name, faculty.Value, cathedra.Value, data.Value, course.Value);
+            }
+        }
+        public void addItems(string name,string faculty,string cathedra,string data,string course)
+        {
+            if (nameBox.Items.Contains(name) == false)
+            {
+                nameBox.Items.Add(name);
+            }
+            if (facultyBox.Items.Contains(faculty) == false)
+            {
+                facultyBox.Items.Add(faculty);
+            }
+            if (cathedraBox.Items.Contains(cathedra) == false)
+            {
+                cathedraBox.Items.Add(cathedra);
+            }
+            if (dataBox.Items.Contains(data) == false)
+            {
+                dataBox.Items.Add(data);
+            }
+            if (courseBox.Items.Contains(course) == false)
+            {
+                courseBox.Items.Add(course);
+            }
+
+        }
+        private void nameBox_Click(object sender, EventArgs e)
+        {
+            NewComboBoxItems();
+        }
+        private void cathedra_Click(object sender, EventArgs e)
+        {
+            NewComboBoxItems();
+        }
+        private void faculty_Click(object sender, EventArgs e)
+        {
+            NewComboBoxItems();
+        }
+        private void data_Click(object sender, EventArgs e)
+        {
+            NewComboBoxItems();
+        }
+        private void course_Click(object sender, EventArgs e)
+        {
+            NewComboBoxItems();
+        }
+        private void nameCheckBox_CheckedChanged(object sender, EventArgs e)
+        {
+            CheckBox Button = (CheckBox)sender;
+            ActiveParametrs.nameActive = Button.Checked;
+        }
+        private void facultyCheckBox_CheckedChanged(object sender, EventArgs e)
+        {
+            CheckBox Button = (CheckBox)sender;
+            ActiveParametrs.facultyActive = Button.Checked;
+        }
+        private void cathedraСheckBox_CheckedChanged(object sender, EventArgs e)
+        {
+            CheckBox Button = (CheckBox)sender;
+            ActiveParametrs.cathedraActive = Button.Checked;
+        }
+        private void dataCheckBox_CheckedChanged(object sender, EventArgs e)
+        {
+            CheckBox Button = (CheckBox)sender;
+            ActiveParametrs.dataActive = Button.Checked;
+        }
+        private void courseCheckBox_CheckedChanged(object sender, EventArgs e)
+        {
+            CheckBox Button = (CheckBox)sender;
+            ActiveParametrs.courseActive = Button.Checked;
+        }
+        private void button2_Click(object sender, EventArgs e)
+        {
+            linqStrategy linq = new linqStrategy();
+            Context context = new Context(linq);
+            context.TransformFile(new People(nameBox.Text, facultyBox.Text, cathedraBox.Text, courseBox.Text, dataBox.Text));
+        }
+    }
+}
